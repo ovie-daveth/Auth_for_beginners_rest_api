3a02f0da3464fa02d6ac90e00f6e0dc3ae507e14
ovie-davet <oviedavid77@mail.com>
2023-05-17 14:29:29 +0100

    Finished with the register client logic, connected to my backend register endpoint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,49 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Profile from './pages/Profile'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import Admin from './pages/Admin'
+import {Toaster} from 'react-hot-toast'
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:8000/api';
 
 const App = () => {
   return (
-    <div>
-      Home
-    </div>
+    <>
+    <Toaster
+  position="top-center"
+  reverseOrder={false}
+  gutter={8}
+  containerClassName=""
+  containerStyle={{}}
+  toastOptions={{
+    // Define default options
+    className: '',
+    duration: 5000,
+    style: {
+      background: '#363636',
+      color: '#fff',
+    },
+
+    // Default options for specific types
+    success: {
+      duration: 3000,
+      theme: {
+        primary: 'green',
+        secondary: 'black',
+      },
+    },
+  }}
+/>
+    <Routes>
+      <Route path="/" element={<Profile />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/admin" element={<Admin />} />
+    </Routes>
+    </>
   )
 }
 
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,105 @@
+import React, { useState } from 'react';
+import axios from "axios"
+import { toast } from 'react-hot-toast';
+
+const Register = () => {
+  const [data, setData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [error, setError] = useState('');
+
+  const { name, email, password } = data;
+
+  const inputs = [
+    {
+      id: 1,
+      value: name,
+      type: 'text',
+      name: 'name',
+    },
+    {
+      id: 2,
+      value: email,
+      type: 'email',
+      name: 'email',
+    },
+    {
+      id: 3,
+      value: password,
+      type: 'password',
+      name: 'password',
+    },
+  ];
+
+  const next = async (e) => {
+    e.preventDefault();
+    if (currentIndex === inputs.length - 1) {  // this checks for the last field eg inputs.length === 3, hence 3-1 == 2 which is how arrays are indexed
+      if (!name || !email || !password) { //it checks if all the field are filled, if not throw an error
+        setError('Please fill in all fields');
+        return;
+      }
+      try {
+        const response = await axios.post("/register", { name, email, password });
+        const res = response.data;
+        if (res.success) {
+          toast.success(res.success);
+          console.log('Form submitted:', res.user);
+        }
+      } catch (error) {
+        if (error.response) {
+          const errorMessage = error.response.data.error; //i used this because inmy server i spcified statuscode 404  which is a server error. so I am passing the server error into my client
+          toast.error(errorMessage);
+        } else {
+          console.error('Form submission error:', error); //normal clint error
+          toast.error(error)
+        }
+      }
+    } else {
+      if (!data[inputs[currentIndex].name]) { //checks if there is data in a particular input field, if no, throw an error. data.[] is how we index an array/object. inputs[currentIndex].name is the name of the attricbute of the cuurent input field tat could be === "email", "name" or "password". so in essence we are simply looping with if ie if !data[email] or !data[name] or !data[password] then do this.
+        setError('Input cannot be empty');
+        return;
+      }
+      setCurrentIndex(currentIndex + 1);
+      setError('');
+    }
+  };
+
+  const inputChange = (id, value) => {
+    const updatedData = { ...data, [inputs[id - 1].name]: value }; // we set the input.name to its value
+    setData(updatedData);
+    setError("")
+  };
+
+
+  return (
+    <div className="bg-black flex flex-col justify-center items-center h-screen">
+      <form className='bg-white shadow-md w-[500px] h-[200px] flex items-center flex-col justify-center gap-5'>
+      <h1 className='font-bold text-lg text-indigo-600 capitalize'>Please Register to continue</h1>
+        {inputs.map((item, index) => (
+          <div
+            key={item.id}
+            //style={{ display: index === currentIndex ? 'flex' : 'none' }}
+            className={`${index === currentIndex ? "flex items-start justify-between" : "hidden"}`}
+          >
+           <div className="flex flex-col ">
+           <input
+              type={item.type}
+              placeholder={`Enter your ${item.name}`}
+              value={item.value}
+              onChange={(e) => inputChange(item.id, e.target.value)}
+              className='bg-indigo-300 text-white font-semibold px-2 py-2 placeholder:text-white placeholder:font-semibold'
+            />
+            {error && index === currentIndex && <p className='text-red-600 italic font-bold'>{error}</p>}
+           </div>
+            <button onClick={next} className='ml-6 bg-indigo-600 py-2 px-5 text-white font-semibold rounded-sm hover:bg-red-500 active:bg-indigo-600'>Next</button>
+          </div>
+        ))}
+      </form>
+    </div>
+  );
+};
+
+export default Register;

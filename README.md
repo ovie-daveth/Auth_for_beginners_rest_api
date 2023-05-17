2555c8e6dde5fb590fe38c6fc5a5257f852fa093
ovie-davet <oviedavid77@mail.com>
2023-05-17 01:44:08 +0100

    Initialised project again

--- README.md
+++
@@ -1,5 +0,0 @@
-# MernAuthCrud
-
-## A Mern PROJECT to test my MERN stack skills
-
-No much consideration for stying, just focus on backend
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,11 @@
-import './App.css';
-import {BrowserRouter, NavLink, Route, Routes } from 'react-router-dom'
-import Register from './pages/Register';
-import Login from './pages/Login';
-import Home from './pages/Home';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <BrowserRouter>
-      <header>
-        <nav className='nav_bar'>
-          <div className='logo'>
-            <h1>Anthena</h1>
-            <small>The best Video Streamer</small>
-          </div>
-          <div className='menus'>
-          <ul>
-          <li><NavLink to="/">Home</NavLink></li>
-            <li><NavLink to="/register">Register</NavLink></li>
-            <li><NavLink to="/login">Login</NavLink></li>
-            <li><NavLink to="/list">List</NavLink></li>
-          </ul>
-          </div>
-        </nav>
-      </header>
-      <div className='container'>
-      
-      </div>
-          <Routes>
-          <Route path="/" element={<Home />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/list" element={<List />} />
-          </Routes>
-      </BrowserRouter>
-
-  );
+    <div>
+      Home
+    </div>
+  )
 }
 
-export default App;
+export default App
